13cdc61d343afc8f1af4e53e27aeb979871da9da
Jacob Zhang-replace <jacobjzhang@gmail.com>
2015-03-11 20:02:54 +0000

    first commit

---
+++ tlstrings.rb
@@ -0,0 +1,23 @@
+puts "Jacob" + " " + "Zhang"
+
+puts 1234 / 1000
+puts 1234 % 1000 / 100
+puts 1234 % 1000 % 100 / 10
+puts 1234 % 1000 % 100 % 10
+
+movies = {Titanic:'1990s', SilverLinings:'2000s'}
+puts movies[:Titanic]
+
+moviesarray = ['1990s', '2000s']
+puts moviesarray[1]
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts 15.0 * 15.6
+puts 39.4 * 13.0
+puts 99.9 * 3.202
+
+# Error message for #7 means you used the wrong closing bracket.
